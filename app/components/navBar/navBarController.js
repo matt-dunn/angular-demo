70e43848b7810a09761152dcd8612c4393219ee1
Matt Dunn <mattdunn@MBP-2014.home>
2015-01-19 19:03:28 +0000

    Removed unused code

--- app/components/navBar/navBarController.js
+++ app/components/navBar/navBarController.js
@@ -6,26 +6,7 @@ define([
     "use strict";
 
     app.register.controller('Components.NavBarController', [
-        '$scope',
-        '$rootScope',
-        function($scope, $rootScope) {
-            var eventListeners = [];
-            $scope.isEditMode = false;
-
-            $scope.setEditMode = function() {
-                $scope.isEditMode = !$scope.isEditMode;
-                $rootScope.$emit('WidgetsController.editMode', {isEditMode: $scope.isEditMode});
-            };
-
-            eventListeners.push($rootScope.$on('WidgetsController.editMode', function (event, data) {
-                $scope.isEditMode = data.isEditMode;
-            }));
-
-            $scope.$on('$destroy', function() {
-                for (var i = 0; i < eventListeners.length; i++) {
-                    eventListeners[i]();
-                }
-            });
+        function() {
         }]
     );
 });
