3c9ad3f224e7e17574bc90e21b592bd377fd7fd7
Matt Dunn <mattdunn@MBP-2014.home>
2015-01-20 09:42:21 +0000

    Controller/view updates

--- app/components/navBar/navBarController.js
+++ app/components/navBar/navBarController.js
@@ -6,7 +6,26 @@ define([
     "use strict";
 
     app.register.controller('Components.NavBarController', [
-        function() {
+        '$scope',
+        '$rootScope',
+        function($scope, $rootScope) {
+            var eventListeners = [];
+            $scope.isEditMode = false;
+
+            $scope.setEditMode = function() {
+                $scope.isEditMode = !$scope.isEditMode;
+                $rootScope.$emit('WidgetsController.editMode', {isEditMode: $scope.isEditMode});
+            };
+
+            eventListeners.push($rootScope.$on('WidgetsController.editMode', function (event, data) {
+                $scope.isEditMode = data.isEditMode;
+            }));
+
+            $scope.$on('$destroy', function() {
+                for (var i = 0; i < eventListeners.length; i++) {
+                    eventListeners[i]();
+                }
+            });
         }]
     );
 });
